34a89b354afa8542693a01139e4893106fb3833f
Samuele Santi <redshadow@hackzine.org>
2014-10-31 17:44:36 +0100

    Humanize all over the place!

--- jobcontrol/web/template_filters.py
+++ jobcontrol/web/template_filters.py
@@ -6,6 +6,9 @@ import time
 filters = {}
 
 
+DEFAULT_DATE_FORMAT = '%a, %d %b %Y %H:%M:%S'
+
+
 def humanize_timestamp(value):
     if value is None:
         return u'never'
@@ -17,8 +20,7 @@ def humanize_timestamp(value):
     if isinstance(value, (int, long, float)):
         return humanize.naturaltime(time.time() - value)
 
-    raise ValueError("Unsupported date format: {0!r}"
-                     .format(value))
+    raise TypeError("Unsupported date format: {0!r}".format(value))
 
 
 def humanize_timedelta(value):
@@ -33,14 +35,27 @@ def humanize_timedelta(value):
         td = datetime.timedelta(seconds=int(value))
         return str(td)
 
-    raise ValueError("Unsupported time delta format: {0!r}"
-                     .format(value))
+    raise TypeError("Unsupported time delta format: {0!r}".format(value))
 
 
 def yesno(value, yes='Yes', no='No'):
     return yes if value else no
 
 
+def strftime(value, fmt=DEFAULT_DATE_FORMAT):
+    if value is None:
+        return 'N/A'
+
+    if isinstance(value, (int, long, float)):
+        value = datetime.datetime.utcfromtimestamp(value)
+
+    if isinstance(value, datetime.datetime):
+        return value.strftime('%a, %d %b %Y %H:%M:%S')
+
+    raise TypeError("Unsupported date object: {0!r}".format(value))
+
+
 filters['humanize_timestamp'] = humanize_timestamp
 filters['humanize_timedelta'] = humanize_timedelta
 filters['yesno'] = yesno
+filters['strftime'] = strftime
