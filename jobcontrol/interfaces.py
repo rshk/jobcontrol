6cd13145e27274a1639bb5b7f470bf281ea49ecf
Samuele Santi <redshadow@hackzine.org>
2014-11-20 17:18:11 +0100

    New interface for the job state storage

--- jobcontrol/interfaces.py
+++ jobcontrol/interfaces.py
@@ -3,60 +3,59 @@ Interfaces for NEW jobcontrol objects.
 
 **Data model**::
 
-    Job     id SERIAL
-    ---     function VARCHAR
-            [OLD] kwargs TEXT (serialized)
-            [OLD] args TEXT (serialized)
+    Job     id TEXT (natural key)
+    ---     title TEXT
+            notes TEXT
+            config TEXT
+                YAML object containing the job configuration:
+                function name, arguments, ...
             ctime TIMESTAMP
             mtime TIMESTAMP
             dependecies INTEGER[] (references Job.id)
-            title TEXT
-            group TEXT
-                The job "group". Note: we might want to use this to build
-                a "tree-style" categorization of jobs; how to handle this?
-                (need to filter on "path" as well)
-                Maybe: http://www.postgresql.org/docs/9.3/static/ltree.html
-                although this requires an extension to be installed..
-            notes TEXT
-            arguments BINARY (PostgreSQL: "bytea")
+                This is copied from config, mostly in order to
+                be able to filter for reverse dependencies.
 
     Build   id SERIAL
     -----   job_id INTEGER (references Job.id)
-            job_config BINARY (PostgreSQL: "bytea" + pickle)
-                Copy of the job configuration, as a dict.
             start_time TIMESTAMP
             end_time TIMESTAMP
             started BOOLEAN
             finished BOOLEAN
             success BOOLEAN
             skipped BOOLEAN
-            [OLD] progress_current INTEGER
-            [OLD] progress_total INTEGER
-            retval BINARY (PostgreSQL: "bytea" + pickle)
-                The return value from the build
-            exception BINARY (PostgreSQL: "bytea" + pickle)
-                The exception which caused the build to fail.
-            traceback BINARY (PostgreSQL: "bytea" + pickle)
-                Object representing a representation of the original
-                exception traceback.
+            job_config TEXT (YAML)
+                Copy of the job configuration whan the build was started
+            build_config TEXT (YAML)
+                Extra configuration, such as dependency build "pinning"
+            retval BINARY (Pickled return value)
+            exception BINARY
+                Pickled exception object (or None)
+            exception_tb BINARY
+                Pickled TracebackInfo object
+
+    Build progress
+    --------------
+            job_id INTEGER (references Job.id)
+            build_id INTEGER (references Build.id)
+            group_name VARCHAR(128)
+                Name of the "progress group" (separated by '::')
+            current INTEGER
+                Current progress value
+            total INTEGER
+                Total progress value
+            status_line TEXT
+                An optional line of text describing current state
+            UNIQUE constraint on (build_id, group_name)
 
     Log     id SERIAL
     ---     job_id INTEGER (references Job.id)
             build_id INTEGER (references Build.id)
             created TIMESTAMP
             level INTEGER
-            record BINARY (PostgreSQL: "bytea" + pickle)
-
-    Progress
-    --------
-            build_id INTEGER (references Build.id)
-            group_name VARCHAR(128)
-                Name of the "progress group" (dotted name)
-            current
-                Current progress value
-            total
-                Total progress value
-            PRIMARY KEY on (build_id, group_name)
+            record BINARY
+                Pickled LogRecord
+            exception_tb BINARY
+                Pickled TracebackInfo object
 
 
 **Function arguments specification:**
@@ -145,35 +144,86 @@ class StorageBase(object):
     # ------------------------------------------------------------
 
     @abc.abstractmethod
-    def create_job(self, function, args=None, kwargs=None, dependencies=None,
-                   title=None):
+    def create_job(self, job_id, function=None, args=None, kwargs=None,
+                   dependencies=None, title=None, notes=None, config=None):
         """
         Create a new job.
 
-        :return: The job id
+        :param job_id:
+            (mandatory) the job "name" (natural key identifier; must be unique;
+            avoid exceeding a "reasonable" length).
+        :param function:
+            Function to be called to run the job
+        :param args:
+            A tuple containing positional arguments.
+            :py:class:`jobcontrol.job_config.Retval` objects may be used
+            to indicate references to dependency job builds return values.
+        :param kwargs:
+            A dictionary containing keyword arguments. Can use Retvals as well.
+        :param dependencies:
+            A list of dependency job names.
+        :param title:
+            An optional title for the job.
+        :param notes:
+            Optional notes for the job.
+        :param config:
+            Optionally, a configuration block (dict), reflecting the
+            structure to be stored in the database.
+            It must be possible to serialize this as YAML, of course.
+        :return:
+            The id of the newly created job
         """
         pass
 
     @abc.abstractmethod
     def update_job(self, job_id, function=None, args=None, kwargs=None,
-                   dependencies=None, title=None):
+                   dependencies=None, title=None, notes=None, config=None):
         """
         Update a job definition.
+
+        Arguments meanings are the same of ``create_job()``.
+        Returns nothing.
         """
         pass
 
+    def _make_config(self, job_id, function, args, kwargs, dependencies, title,
+                     notes, config=None):
+        """Utility function to merge arguments into configuration"""
+        _config = {
+            'id': None,
+            'function': None,
+            'args': (),
+            'kwargs': {},
+            'dependencies': [],
+            'title': None,
+            'notes': None,
+        }
+        if config is not None:
+            _config.update(config)
+        if job_id is not None:
+            _config['id'] = job_id
+        if function is not None:
+            _config['function'] = function
+        if args is not None:
+            _config['args'] = args
+        if kwargs is not None:
+            _config['kwargs'] = kwargs
+        if dependencies is not None:
+            _config['dependencies'] = dependencies
+        if title is not None:
+            _config['title'] = title
+        if notes is not None:
+            _config['notes'] = notes
+        return _config
+
     @abc.abstractmethod
     def get_job(self, job_id):
-        """
-        Get a job definition, as a dict, by id.
-        """
+        """Get a job definition, as a dict, by id"""
         pass
 
     @abc.abstractmethod
     def delete_job(self, job_id):
-        """
-        Delete a job definition, by id.
-        """
+        """Delete a job definition, by id"""
         pass
 
     @abc.abstractmethod
@@ -182,28 +232,33 @@ class StorageBase(object):
         pass
 
     def iter_jobs(self):
-        """
-        Iterate all jobs, yielding them as dicts.
-        """
+        """Iterate all jobs, yielding them as dicts"""
         for id in self.job_list():
             yield self.job_get(id)
 
     def mget_jobs(self, job_ids):
         """
-        Get multiple job definitions, by id.
+        Get multiple job definitions, by id(s).
 
         Especially useful for getting dependencies.
+
+        .. note::
+
+            The default implementation just falls back to getting the jobs
+            one by one, which will work, but will usually be sub-optimal.
+
+        :param job_ids: A list of job ids.
         """
         return [self.job_get(x) for x in job_ids]
 
     @abc.abstractmethod
     def get_job_deps(self, job_id):
-        """Get direct job dependencies"""
+        """Get ids of direct job dependencies"""
         pass
 
     @abc.abstractmethod
     def get_job_revdeps(self, job_id):
-        """Get jobs directly depending on this one"""
+        """Get ids of jobs directly depending on this one"""
         pass
 
     @abc.abstractmethod
@@ -235,10 +290,16 @@ class StorageBase(object):
     # ------------------------------------------------------------
 
     @abc.abstractmethod
-    def create_build(self, job_id):
+    def create_build(self, job_id, build_config=None):
         """
         Create a build.
 
+        :param job_id:
+            The job for which a build should be started
+
+        :param build_config:
+            Build configuration, containing things like dependency build
+            pinning, etc.
         :return: the build id
         """
         pass
@@ -275,13 +336,32 @@ class StorageBase(object):
         pass
 
     @abc.abstractmethod
-    def update_build_progress(self, build_id, current, total):
+    def report_build_progress(self, build_id, current, total, group_name='',
+                              status_line=''):
         """
-        Update the current progress for a given build.
+        Report progress for a build.
+
+        :param build_id:
+        :param current:
+            The current number of "steps" done
+        :param total:
+            The total amount of "steps"
+        :param group_name:
+            Optionally, a name used to nest multiple progress "levels".
+            A tuple (or string separated by '::' can be used to specify
+            multiple "nesting" levels)
+        :param status_line:
+            Optionally, a line of text indicating the current build status.
         """
         pass
 
     def get_latest_successful_build(self, job_id):
+        """
+        Helper method to retrieve the latest successful build for a given
+        job. Calls ``get_job_builds()`` in the background.
+
+        :return: information about the build, as a dict
+        """
         builds = list(self.get_job_builds(
             job_id, started=True, finished=True, success=True, skipped=False,
             order='desc', limit=1))
@@ -293,15 +373,18 @@ class StorageBase(object):
     @abc.abstractmethod
     def log_message(self, job_id, build_id, record):
         """
-        Store a log record for a build
+        Store a log record associated with a build.
         """
+        # Todo: provide "shortcut" methods to convert the traceback to
+        #       a serializable object, and to clean up the record
+        #       object for decent serialization in the database.
         pass
 
     @abc.abstractmethod
     def prune_log_messages(self, job_id=None, build_id=None, max_age=None,
                            level=None):
         """
-        Delete old log messages.
+        Delete (old) log messages.
 
         :param job_id:
             If specified, only delete messages for this job
@@ -323,7 +406,7 @@ class StorageBase(object):
     def iter_log_messages(self, job_id=None, build_id=None, max_date=None,
                           min_date=None, min_level=None):
         """
-        Iterate log messages.
+        Iterate over log messages, applying some filters.
 
         :param job_id:
             If specified, only return messages for this job
