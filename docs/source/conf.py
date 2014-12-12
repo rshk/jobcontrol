e3e8d583091285e2081bff0d457906d9b7780a7d
Samuele Santi <redshadow@hackzine.org>
2014-12-12 14:49:22 +0100

    Updated copyright notice in documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'JobControl'
-copyright = u'2014, Samuele Santi'
+copyright = u'2014, Samuele Santi - Trento Rise'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -235,7 +235,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     ('index', 'JobControl.tex', u'JobControl Documentation',
-     u'Samuele Santi', 'manual'),
+     u'Samuele Santi - Trento Rise', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -265,7 +265,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'jobcontrol', u'JobControl Documentation',
-     [u'Samuele Santi'], 1)
+     [u'Samuele Santi - Trento Rise'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -279,7 +279,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     ('index', 'JobControl', u'JobControl Documentation',
-     u'Samuele Santi', 'JobControl', 'One line description of project.',
+     u'Samuele Santi - Trento Rise', 'JobControl', 'One line description of project.',
      'Miscellaneous'),
 ]
 
